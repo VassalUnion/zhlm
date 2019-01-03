790aa91e12dd13a0bd24144378958617d0105a67
root <root@coding.ide>
2019-01-03 12:42:05 +0000

    blog update

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Thu Jan 03 2019 12:24:59 GMT+0000 (UTC)
+// build time:Thu Jan 03 2019 12:42:04 GMT+0000 (UTC)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
