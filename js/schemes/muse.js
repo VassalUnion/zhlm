4da4d5f2e319fc56b5ad69f0b9b04506362e4942
zhoufangyuanPT <zhoufangyuanPT@126.com>
2019-07-08 15:17:14 +0800

    blog update

--- js/schemes/muse.js
+++ js/schemes/muse.js
@@ -1,3 +1,3 @@
-// build time:Sat Jul 06 2019 11:12:39 GMT+0800 (CST)
+// build time:Mon Jul 08 2019 15:17:13 GMT+0800 (CST)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 
