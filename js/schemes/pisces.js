f4175662d6f2e396958e88bb1737501e959ba88a
zhoufangyuanPT <zhoufangyuanPT@126.com>
2019-08-13 14:51:24 +0800

    blog update

--- js/schemes/pisces.js
+++ js/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Tue Aug 13 2019 14:32:44 GMT+0800 (CST)
+// build time:Tue Aug 13 2019 14:51:23 GMT+0800 (CST)
 $(document).on("DOMContentLoaded",function(){var t=$(".sidebar-inner");var e=CONFIG.sidebar.offset||12;function o(){return $(".header-inner").height()+e}function i(){var t=$("#footer");var e=$(".footer-inner");var o=t.outerHeight()-e.outerHeight();var i=t.outerHeight()+o;return i}function n(){var n=o();var a=i();t.affix({offset:{top:n-e,bottom:a}});$("#sidebar").css({"margin-top":n,"margin-left":"auto"})}function a(){$(window).off(".affix");t.removeData("bs.affix").removeClass("affix affix-top affix-bottom");n()}function r(){var t=window.matchMedia("(min-width: 992px)");t.addListener(function(t){if(t.matches){a()}})}n();r()});
 //rebuild by neat 
